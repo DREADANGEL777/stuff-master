c341652d2d360aec675a4a0f4ebdabf186eb12f7
DREADANGEL777 <dreadangel27@gmail.com>
2024-08-05 17:46:32 +0300

    working products with filtered

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -3,6 +3,7 @@ import axios from "axios"
 
 import { BASE_URL } from "../../utils/constants"
 
+
 export const getProducts = createAsyncThunk("products/getProducts", async (_, thunkAPI) => {
   try {
     const res = await axios(`${BASE_URL}/products`)
@@ -21,7 +22,15 @@ const productsSlice = createSlice({
     related: [],
     isLoading: false,
   },
-
+  reducers: {
+    filterByPrice: (state, { payload }) => {
+      state.filtered = state.list.filter(({ price }) => price < payload)
+    },
+    getRelatedProducts: (state, { payload }) => {
+      const list = state.list.filter(({ category: { id } }) => id === payload)
+      state.related = (list)
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(getProducts.pending, (state) => {
       state.isLoading = true
