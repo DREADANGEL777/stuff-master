e80e41add5d020f85b3f2c9f9c27f010c693afbd
DREADANGEL777 <dreadangel27@gmail.com>
2024-08-05 17:38:10 +0300

    working banner and categories

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,20 @@
 import React from "react"
-import Poster from "../Poster/Poster"
 import { useSelector } from "react-redux"
 
+import Poster from "../Poster/Poster"
 import Products from "../Products/Products"
+import Categories from "../Categories/Categories"
+import Banner from "../Banner/Banner"
 
 const Home = () => {
-  const { list } = useSelector(({ products }) => products)
+  const { products, categories } = useSelector((state) => state)
 
   return (
     <>
       <Poster />
-      <Products products={list} amount={5} title="Trending" />
+      <Products products={products.list} amount={5} title="Trending" />
+      <Categories products={categories.list} amount={5} title="Worth seeing" />
+      <Banner />
     </>
   )
 }
